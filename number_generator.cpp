336f3946bb8e75f3b8672ace7ec0111712e67de7
MANDRW <mandrzejewski132@gmail.com>
2024-04-02 21:28:12 +0200

    measurements

--- number_generator.cpp
+++ number_generator.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main(){
     fstream file;
     srand(time(NULL));
-    file.open("test_data_small.txt",ios::out);
+    file.open("test_data_small3.txt",ios::out);
     for(int i=0;i<100000;i++) file<<(rand()%21)-10<<endl;
     file.close();
-    file.open("test_data_big.txt",ios::out);
+    file.open("test_data_big3.txt",ios::out);
     for(int i=0;i<100000;i++) file<<(rand()%999000001)+1000000<<endl;
     file.close();
     return 0;
